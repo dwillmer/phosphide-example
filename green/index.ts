72eefe61ca45f54c159eb8bbd2d9c7a29ac37c5b
Steven Silvester <steven.silvester@ieee.org>
2015-12-11 14:36:14 -0600

    Initial implementation

---
+++ green/index.ts
@@ -0,0 +1,44 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+'use strict';
+
+import {
+  IShellView
+} from 'phosphide';
+
+import * as di
+  from 'phosphor-di';
+
+import {
+  Widget
+} from 'phosphor-widget';
+
+
+export
+function resolve(): Promise<void> {
+  return di.resolve(Plugin).then(plugin => { plugin.run(); });
+}
+
+
+class Plugin {
+
+  static requires = [IShellView];
+
+  constructor(shell: IShellView) {
+    this._shell = shell;
+  }
+
+  run(): void {
+    let view = new Widget();
+    view.addClass('green-content');
+    view.title.text = 'Green';
+    this._shell.addRightView(view);
+  }
+
+  private _shell: IShellView;
+}
